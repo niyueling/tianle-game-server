8b4511ccd1804eed17a9b8d7eceb5cef6497ac3a
我爱蓝莓 <56059199@qq.com>
2024-01-16 15:26:28 +0800

    更新牌型

--- src/match/base/newRobotManager.ts
+++ src/match/base/newRobotManager.ts
@@ -340,7 +340,7 @@ export class NewRobotManager {
           console.warn(proxy.model._id.toString(), proxy.model.shortId, this.room.readyPlayers);
           // 有在线用户没点下一局
           console.log(`human player ${proxy.model.shortId} not ready in room ${this.room._id}`);
-          // await this.room.ready(proxy);
+          await this.room.ready(proxy);
           return false;
         }
       }
