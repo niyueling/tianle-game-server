5b2fe2b4c78e00668a9a9088dc0ae889617a31e2
我爱蓝莓 <56059199@qq.com>
2024-01-17 17:01:59 +0800

    解决打牌偶发性多发牌问题

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1425,22 +1425,19 @@ class TableState implements Serializable {
       if (xiajia) {
         console.warn(`xiajia: ${xiajia.model.shortId}, index: ${this.players.indexOf(xiajia)}`);
 
-        const env = {card, from, turn: this.turn}
-        this.actionResolver = new ActionResolver(env, () => {
-          const newCard = this.consumeCard(xiajia);
-          const msg = xiajia.takeCard(this.turn, newCard);
+        const newCard = this.consumeCard(xiajia);
+        const msg = xiajia.takeCard(this.turn, newCard);
 
-          if (!msg) {
-            console.error("consume card error msg ", msg);
-            this.room.broadcast('game/game-error', {ok: false, data: {name: "game/takeCard", msg: "consume card error msg"}}, xiajia.msgDispatcher);
-            return;
-          }
-          this.state = stateWaitDa;
-          this.stateData = {da: xiajia, card: newCard, msg};
-          const sendMsg = {index: this.players.indexOf(xiajia)}
-          this.room.broadcast('game/oppoTakeCard', {ok: true, data: sendMsg}, xiajia.msgDispatcher);
-          logger.info('da broadcast game/oppoTakeCard  msg %s', JSON.stringify(sendMsg), "remainCard", this.remainCards);
-        })
+        if (!msg) {
+          console.error("consume card error msg ", msg);
+          this.room.broadcast('game/game-error', {ok: false, data: {name: "game/takeCard", msg: "consume card error msg"}}, xiajia.msgDispatcher);
+          return;
+        }
+        this.state = stateWaitDa;
+        this.stateData = {da: xiajia, card: newCard, msg};
+        const sendMsg = {index: this.players.indexOf(xiajia)}
+        this.room.broadcast('game/oppoTakeCard', {ok: true, data: sendMsg}, xiajia.msgDispatcher);
+        logger.info('da broadcast game/oppoTakeCard  msg %s', JSON.stringify(sendMsg), "remainCard", this.remainCards);
       } else {
         this.room.broadcast('game/game-error', {ok: false, data: {name: "game/takeCard", msg: "No unbroke player found as the next player"}});
         return console.warn('No unbroke player found as the next player');
