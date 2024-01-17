89b3f3bdc303ca01b15f23b0fb71684234fcd66a
我爱蓝莓 <56059199@qq.com>
2024-01-17 14:30:47 +0800

    加日志

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1158,6 +1158,7 @@ class TableState implements Serializable {
           const ok = player.zimo(card, turn === 1, this.remainCards === 0);
           if (ok && player.daHuPai(card, null)) {
             this.lastDa = player;
+            from = this.atIndex(this.lastDa);
             player.sendMessage('game/huReply', {
               ok: true,
               data: {
