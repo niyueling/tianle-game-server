8a81abb5fcec734a3ae6f2549b7420b610ddc95b
我爱蓝莓 <56059199@qq.com>
2024-01-23 17:39:17 +0800

    修复打牌失败

--- src/match/majiang/robotRmqProxy.ts
+++ src/match/majiang/robotRmqProxy.ts
@@ -14,9 +14,6 @@ export class MJRobotRmqProxy extends RobotRmqProxy {
       // 从牌堆中取出合适的牌
       const index = this.room.gameState.promptWithPattern(this.playerState, this.room.gameState.lastTakeCard);
       console.log(`moCard: ${index}, count: ${this.playerState.cards[index]}`)
-      if (this.playerState.cards[index] <= 0) {
-
-      }
       await this.room.gameState.onPlayerDa(this.playerState, this.room.gameState.turn, index);
     }
   }
