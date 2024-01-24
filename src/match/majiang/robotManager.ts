de9d9e0f5784793ff406e677dbad7e92330e424b
我爱蓝莓 <56059199@qq.com>
2024-01-24 14:04:37 +0800

    复活礼包停住整个对局

--- src/match/majiang/robotManager.ts
+++ src/match/majiang/robotManager.ts
@@ -66,7 +66,8 @@ export class RobotManager extends NewRobotManager {
       const AnGangIndex = this.isPlayerAnGang(proxy.playerState);
       const buGangIndex = this.isPlayerBuGang(proxy.playerState);
       const isHu = proxy.playerState.checkZiMo();
-      if (this.room.gameState.state !== 9) {
+      console.warn("game-state:", this.room.gameState.state);
+      if (this.room.gameState.state !== 10) {
         if (this.isPlayerGang(playerId) && this.room.gameState.state === 2) {
           await proxy.gang(this.isPlayerGang(playerId))
         } else if (this.isPlayerChoice(playerId) && this.room.gameState.state === 2) {
