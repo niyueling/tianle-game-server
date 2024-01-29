81400e950f24416af5404210b2f10e741f027f27
我爱蓝莓 <56059199@qq.com>
2024-01-29 15:50:06 +0800

    重连下发特殊牌分数

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -2269,11 +2269,13 @@ class TableState implements Serializable {
         msg = this.players[i].genSelfStates(i);
         msg.roomRubyReward = roomRubyReward;
         msg.constellationCards = this.players[i].constellationCards;
+        msg.constellationCardLevel = this.calcConstellationCardScore(this.players[i]);
         pushMsg.status.push(msg)
       } else {
         msg = this.players[i].genOppoStates(i);
         msg.roomRubyReward = roomRubyReward;
         msg.constellationCards = this.players[i].constellationCards;
+        msg.constellationCardLevel = this.calcConstellationCardScore(this.players[i]);
         pushMsg.status.push(msg)
       }
     }
