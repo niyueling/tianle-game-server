94ff05c8a628f0139809f65182aa3c35af6d0672
我爱蓝莓 <56059199@qq.com>
2024-02-23 18:29:33 +0800

    加入房间下发头像框和称号

--- src/match/majiang/room.ts
+++ src/match/majiang/room.ts
@@ -545,23 +545,24 @@ class Room extends RoomBase {
   }
 
   async joinMessageFor(newJoinPlayer): Promise<any> {
-    const newModel = JSON.parse(JSON.stringify(newJoinPlayer.model));
+    let medalId = null;
+    let headerBorderId = null;
     // 获取用户称号
     const playerMedal = await PlayerMedal.findOne({playerId: newJoinPlayer._id, isUse: true});
     if (playerMedal && (playerMedal.times === -1 || playerMedal.times > new Date().getTime())) {
-      newModel.medalId = playerMedal.propId;
+      medalId = playerMedal.propId;
     }
 
     // 获取用户头像框
     const playerHeadBorder = await PlayerHeadBorder.findOne({playerId: newJoinPlayer._id, isUse: true});
     if (playerHeadBorder && (playerHeadBorder.times === -1 || playerHeadBorder.times > new Date().getTime())) {
-      newModel.headerBorderId = playerHeadBorder.propId;
+      headerBorderId = playerHeadBorder.propId;
     }
 
     return {
       _id: this._id,
       index: this.indexOf(newJoinPlayer),
-      model: newModel,
+      model: {...newJoinPlayer.model, medalId, headerBorderId},
       ip: newJoinPlayer.getIpAddress(),
       location: newJoinPlayer.location,
       owner: this.ownerId,
