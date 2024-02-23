605380314cbfbb937f5982cc194c41f6c30c13c7
我爱蓝莓 <56059199@qq.com>
2024-02-23 18:13:44 +0800

    结算扣豆延迟1秒下发,胡牌延迟1秒摸牌

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1484,7 +1484,7 @@ class TableState implements Serializable {
                       }
                     }
 
-                    setTimeout(nextDo, 1500);
+                    setTimeout(nextDo, 2000);
                   } else {
                     const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
                     const nextZhuang = this.nextZhuang()
@@ -1605,7 +1605,7 @@ class TableState implements Serializable {
                   }
                 }
 
-                setTimeout(nextDo, 1500);
+                setTimeout(nextDo, 2000);
               } else {
                 // console.warn('No unbroke player found as the next player but last da %s', this.atIndex(this.lastDa));
                 const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
@@ -2167,7 +2167,7 @@ class TableState implements Serializable {
         this.room.broadcast("game/playerChangeGold", {ok: true, data: playersModifyGolds});
       }
 
-      setTimeout(nextDo, 1000);
+      setTimeout(nextDo, 1500);
 
 
       const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
