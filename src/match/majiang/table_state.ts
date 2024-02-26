dae4fcd968158ff06fca32d628b0b75cec6cd8ac
我爱蓝莓 <56059199@qq.com>
2024-02-26 16:29:50 +0800

    修改打牌

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1855,45 +1855,45 @@ class TableState implements Serializable {
         }
       }
 
-      if (xiajia) {
-        console.warn(`xiajia: ${xiajia.model.shortId}, index: ${this.players.indexOf(xiajia)} cradLength: ${this.checkPlayerCount(xiajia)}`);
-
-        const newCard = await this.consumeCard(xiajia);
-        if (newCard) {
-          const msg = xiajia.takeCard(this.turn, newCard);
-
-          if (!msg) {
-            console.error("consume card error msg ", msg);
-            this.room.broadcast('game/game-error', {
-              ok: false,
-              data: {name: "game/takeCard", msg: "consume card error msg"}
-            }, xiajia.msgDispatcher);
-            return;
-          }
-
-          this.state = stateWaitDa;
-          this.stateData = {da: xiajia, card: newCard, msg};
-          const sendMsg = {index: this.players.indexOf(xiajia)};
-          this.room.broadcast('game/oppoTakeCard', {ok: true, data: sendMsg}, xiajia.msgDispatcher);
-          logger.info('da broadcast game/oppoTakeCard  msg %s', JSON.stringify(sendMsg), "remainCard", this.remainCards);
-        }
-      } else {
-        this.room.broadcast('game/game-error', {
-          ok: false,
-          data: {
-            name: "game/takeCard",
-            msg: "No unbroke player found as the next player",
-            data: {players: this.players, isFaPai: this.isFaPai}
-          }
-        });
-
-        const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
-        const nextZhuang = this.nextZhuang()
-        await this.gameAllOver(states, [], nextZhuang);
-      }
-
       const env = {card, from, turn: this.turn}
-      this.actionResolver = new ActionResolver(env, async () => {});
+      this.actionResolver = new ActionResolver(env, async () => {
+        if (xiajia) {
+          console.warn(`xiajia: ${xiajia.model.shortId}, index: ${this.players.indexOf(xiajia)} cradLength: ${this.checkPlayerCount(xiajia)}`);
+
+          const newCard = await this.consumeCard(xiajia);
+          if (newCard) {
+            const msg = xiajia.takeCard(this.turn, newCard);
+
+            if (!msg) {
+              console.error("consume card error msg ", msg);
+              this.room.broadcast('game/game-error', {
+                ok: false,
+                data: {name: "game/takeCard", msg: "consume card error msg"}
+              }, xiajia.msgDispatcher);
+              return;
+            }
+
+            this.state = stateWaitDa;
+            this.stateData = {da: xiajia, card: newCard, msg};
+            const sendMsg = {index: this.players.indexOf(xiajia)};
+            this.room.broadcast('game/oppoTakeCard', {ok: true, data: sendMsg}, xiajia.msgDispatcher);
+            logger.info('da broadcast game/oppoTakeCard  msg %s', JSON.stringify(sendMsg), "remainCard", this.remainCards);
+          }
+        } else {
+          this.room.broadcast('game/game-error', {
+            ok: false,
+            data: {
+              name: "game/takeCard",
+              msg: "No unbroke player found as the next player",
+              data: {players: this.players, isFaPai: this.isFaPai}
+            }
+          });
+
+          const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
+          const nextZhuang = this.nextZhuang()
+          await this.gameAllOver(states, [], nextZhuang);
+        }
+      });
 
       for (let j = 1; j < this.players.length; j++) {
         const i = (index + j) % this.players.length
