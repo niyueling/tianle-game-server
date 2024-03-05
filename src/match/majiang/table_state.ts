9e16e4540a7a785906116b39507059b0506a3d04
我爱蓝莓 <56059199@qq.com>
2024-03-05 14:51:16 +0800

    取消多人胡牌

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1961,6 +1961,7 @@ class TableState implements Serializable {
           p.record('choice', card, msg)
           // 碰、杠等
           p.sendMessage('game/canDoSomething', {ok: true, data: msg});
+          break;
         }
       }
 
