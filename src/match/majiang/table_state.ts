17caae03dec5d5ffb13f69f443debe3b195b9c7a
我爱蓝莓 <56059199@qq.com>
2024-03-05 13:59:54 +0800

    胡牌后不能进行碰牌

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -1934,12 +1934,12 @@ class TableState implements Serializable {
       }
 
       if (check[Enums.pengGang]) {
-        if (check[Enums.peng]) this.actionResolver.appendAction(check[Enums.peng], 'peng')
+        if (check[Enums.peng] && !check[Enums.peng].isGameHu) this.actionResolver.appendAction(check[Enums.peng], 'peng');
         if (check[Enums.gang]) {
-          const p = check[Enums.gang]
-          const gangInfo = [card, p.getGangKind(card, p._id.toString() === player.model._id.toString())]
-          p.gangForbid.push(card)
-          this.actionResolver.appendAction(check[Enums.gang], 'gang', gangInfo)
+          const p = check[Enums.gang];
+          const gangInfo = [card, p.getGangKind(card, p._id.toString() === player.model._id.toString())];
+          p.gangForbid.push(card);
+          this.actionResolver.appendAction(check[Enums.gang], 'gang', gangInfo);
         }
       }
 
