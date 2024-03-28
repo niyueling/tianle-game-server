4495d3f21549290746eb12773c0103c60e2b618b
我爱蓝莓 <56059199@qq.com>
2024-03-28 10:44:34 +0800

    开始对局后真实用户未准备自动准备

--- src/match/base/newRobotManager.ts
+++ src/match/base/newRobotManager.ts
@@ -344,7 +344,7 @@ export class NewRobotManager {
       isOffline = this.isHumanPlayerOffline(proxy);
       if (!isOffline) {
         // 在线用户且非机器人
-        index = this.room.readyPlayers.findIndex(proxy.model._id.toString());
+        index = this.room.readyPlayers.findIndex(proxy.model._id);
         console.warn(index);
         if (index === -1) {
           // 有在线用户没点下一局
