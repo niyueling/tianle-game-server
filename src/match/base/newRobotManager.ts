854fbf41a73224b17e67c523eef963bdc7fe5110
我爱蓝莓 <56059199@qq.com>
2024-03-28 10:31:43 +0800

    开始对局后真实用户未准备自动准备

--- src/match/base/newRobotManager.ts
+++ src/match/base/newRobotManager.ts
@@ -347,7 +347,7 @@ export class NewRobotManager {
         index = this.room.readyPlayers.indexOf(proxy.model._id)
         if (index === -1) {
           // 有在线用户没点下一局
-          console.log(`human player ${proxy.model.shortId} not ready in room ${this.room._id} readyPlayers ${JSON.stringify(this.room.readyPlayers)} playerId ${proxy.model._id}`);
+          console.log(`human player ${proxy.model.shortId} not ready in room ${this.room._id} readyPlayers ${JSON.stringify(this.room.readyPlayers)} playerId ${proxy.model._id} index ${index}`);
           if (this.room.gameState) {
             await this.room.ready(proxy);
           }
