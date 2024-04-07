86ce5d8b23a2f0053ffea29a62c05f0d05d05146
我爱蓝莓 <56059199@qq.com>
2024-04-07 13:40:41 +0800

    修复重连bug

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -5261,7 +5261,7 @@ class TableState implements Serializable {
     switch (this.state) {
       case stateWaitDa: {
         const daPlayer = this.stateData[Enums.da];
-        // console.warn("this.stateData.msg-%s daPlayerId-%s playerId-%s", JSON.stringify(this.stateData.msg), daPlayer._id.toString(), player._id.toString());
+        console.warn("this.stateData.msg-%s daPlayerIndex-%s playerId-%s", JSON.stringify(this.stateData.msg), this.atIndex(daPlayer), this.atIndex(player));
         if (daPlayer._id.toString() === player._id.toString()) {
           pushMsg.current = {
             index,
