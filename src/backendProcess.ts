aa44520aa6462039300ebabfaf775152c808ed66
我爱蓝莓 <56059199@qq.com>
2024-04-10 11:01:25 +0800

    血流红中修改发牌

--- src/backendProcess.ts
+++ src/backendProcess.ts
@@ -74,6 +74,7 @@ export class BackendProcess {
 
       const unfinishedRoomId = await service.roomRegister.getDisconnectedRoom(messageBody.from, this.gameName);
       if (unfinishedRoomId) {
+        console.warn("unfinishedRoomId-%s", unfinishedRoomId);
         return this.sendMessage('room/createReply', {ok: false, info: TianleErrorCode.roomIsNotFinish}, playerRouteKey);
       }
 
@@ -81,6 +82,7 @@ export class BackendProcess {
       if (playerModel) {
         const alreadyInRoom = await service.roomRegister.roomNumber(playerModel._id, this.gameName)
         if (alreadyInRoom) {
+          console.warn("alreadyInRoom-%s", unfinishedRoomId);
           return this.sendMessage('room/createReply', {ok: false, info: TianleErrorCode.roomIsNotFinish}, playerRouteKey);
         }
 
