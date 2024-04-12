a6e5361cef07a2b14f80420d8e7a8a4eb2d1a924
我爱蓝莓 <56059199@qq.com>
2024-04-12 18:22:29 +0800

    修改机器人逻辑

--- src/match/majiang/robotManager.ts
+++ src/match/majiang/robotManager.ts
@@ -61,19 +61,20 @@ export class RobotManager extends NewRobotManager {
       } else if (isPlayerChoice && this.room.gameState.state === 2) {
         await proxy.choice(isPlayerChoice)
       } else if (isPlayerDa) {
-        if (this.waitInterval[key] >= this.getWaitSecond()) {
-          this.waitInterval[key] = 0;
+        if (ziMoHu.hu) {
+          await proxy.choice(Enums.hu)
+        } else if (AnGangIndex && !this.room.gameState.isAllHu) {
+          await proxy.gang(Enums.anGang, AnGangIndex)
+        } else if (buGangIndex && !this.room.gameState.isAllHu) {
+          await proxy.gang(Enums.buGang, buGangIndex)
+        } else {
+          if (this.waitInterval[key] >= this.getWaitSecond()) {
+            this.waitInterval[key] = 0;
 
-          if (ziMoHu.hu) {
-            await proxy.choice(Enums.hu)
-          } else if (AnGangIndex && !this.room.gameState.isAllHu) {
-            await proxy.gang(Enums.anGang, AnGangIndex)
-          } else if (buGangIndex && !this.room.gameState.isAllHu) {
-            await proxy.gang(Enums.buGang, buGangIndex)
-          } else {
             await proxy.playCard();
           }
         }
+
       } else {
         // 过
         if (this.isPlayerGuo(playerId)) {
