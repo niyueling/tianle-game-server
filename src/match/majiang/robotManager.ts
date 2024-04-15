ecd317844ce1e6ce8d4d5724d1a81c853f43f3ca
我爱蓝莓 <56059199@qq.com>
2024-04-15 20:50:17 +0800

    修改机器人逻辑

--- src/match/majiang/robotManager.ts
+++ src/match/majiang/robotManager.ts
@@ -60,7 +60,8 @@ export class RobotManager extends NewRobotManager {
       const isPlayerChoice = this.isPlayerChoice(playerId, jiePaoHu);
       const isPlayerGang = this.isPlayerGang(playerId);
       if (this.room.gameState.state === 2 && jiePaoHu.hu) {
-        console.log("playerId-%s index-%s state-%s isPlayerDa-%s isPlayerChoice-%s jiePaoHu-%s", playerId, this.room.gameState.atIndex(proxy.playerState), this.room.gameState.state, isPlayerDa, isPlayerChoice, JSON.stringify(jiePaoHu));
+        console.log("playerId-%s index-%s state-%s waitInterval-%s isPlayerDa-%s isPlayerChoice-%s jiePaoHu-%s", playerId, this.room.gameState.atIndex(proxy.playerState),
+          this.room.gameState.state, this.waitInterval[key], isPlayerDa, isPlayerChoice, JSON.stringify(jiePaoHu));
       }
 
       if (this.waitInterval[key] >= this.getWaitSecond()) {
