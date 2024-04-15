1db171a5c1906fcc25eee867fa29199321347703
我爱蓝莓 <56059199@qq.com>
2024-04-15 17:35:57 +0800

    修改机器人逻辑

--- src/match/majiang/table_state.ts
+++ src/match/majiang/table_state.ts
@@ -4792,7 +4792,7 @@ class TableState implements Serializable {
         this.room.broadcast("game/playerChangeGold", {ok: true, data: playersModifyGolds});
       }
 
-      setTimeout(nextDo, this.cardTypes.cardId >= 45 ? 3000 : 1500);
+      setTimeout(nextDo, this.cardTypes.cardId >= 45 ? 3500 : 1500);
 
       const states = this.players.map((player, idx) => player.genGameStatus(idx, 1))
       const nextZhuang = this.nextZhuang()
