7d08ff60b3586b00fb264020bd5860065631c6d9
我爱蓝莓 <56059199@qq.com>
2024-04-22 19:29:56 +0800

    修改退税

--- src/match/xueliu/player_state.ts
+++ src/match/xueliu/player_state.ts
@@ -983,7 +983,8 @@ class PlayerState implements Serializable {
       this.cards.alreadyTakenCard = this.alreadyTakenCard
 
       const checkResult = HuPaiDetect.check(this.cards, this.events, this.rule, this.seatIndex)
-      console.warn("check zimo card-%s, cards-%s checkResult-%s", card, JSON.stringify(this.getCardList(this.cards)), JSON.stringify(checkResult));
+      const checkZimo = this.checkZiMo();
+      console.warn("check zimo card-%s, cards-%s zimo-%s checkZiMo-%s", card, JSON.stringify(this.getCardList(this.cards)), checkResult.hu, checkZimo.hu);
       if (checkResult.hu) {
         checkResult.zhuang = this.zhuang
 
