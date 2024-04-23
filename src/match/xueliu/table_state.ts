6bdd5a8558186ccc1730fac9bfef971dee8f157b
我爱蓝莓 <56059199@qq.com>
2024-04-23 11:32:27 +0800

    修改血流红中牌型

--- src/match/xueliu/table_state.ts
+++ src/match/xueliu/table_state.ts
@@ -986,7 +986,7 @@ class TableState implements Serializable {
         }
 
         if (player.zhuang) {
-          console.warn("start-%s, end-%s, cardList-%s state-%s", i * 10 + j, i * 10 + j + 6, JSON.stringify(cardList), state);
+          console.warn("start-%s, end-%s, cardList-%s, cards-%s, state-%s", i * 10 + j, i * 10 + j + 6, JSON.stringify(cardList), JSON.stringify(this.getCardArray(cards)), state);
         }
 
         if (state) {
