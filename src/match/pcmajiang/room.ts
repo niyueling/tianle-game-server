4f986dcb970cf3f79d790ed7196a8b48b31c50f2
我爱蓝莓 <56059199@qq.com>
2024-05-20 10:44:41 +0800

    加入房间获取用户积分

--- src/match/pcmajiang/room.ts
+++ src/match/pcmajiang/room.ts
@@ -375,7 +375,7 @@ class Room extends RoomBase {
   getScore(player) {
     // console.warn("seatIndex-%s, quanFei-%s, feiNiao-%s, gameScore-%s, balance-%s, diFen-%s", player.seatIndex, this.rule.quanFei > 0,
     //   this.rule.feiNiao > 0, player.gameScore, player.balance, player.gameDiFen)
-    console.warn(JSON.stringify(player))
+    console.warn("playerInfo-%s", JSON.stringify(player))
     return (this.rule.quanFei > 0 || this.rule.feiNiao > 0) ? player.gameScore : player.balance
   }
 
