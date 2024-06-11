9886eaa19f3a6f18a2eb6fa970957c5c92b26555
我爱蓝莓 <56059199@qq.com>
2024-06-11 11:49:28 +0800

    修复厦门麻将金豆房不扣金豆bug

--- src/match/xmmajiang/auditManager.ts
+++ src/match/xmmajiang/auditManager.ts
@@ -125,7 +125,7 @@ export class AuditManager {
 
         // 已经出过了, 检查自己有没有一张这种牌
         console.warn("card-%s, cardCount-%s, cardCount1-%s, seatIndex-%s, cardUsed-%s", i, this.model.playerCardList[playerId][i], cards[i], seatIndex, this.model.cardUsed[i]);
-        if (cards[i] === 1 && this.model.playerCardList[playerId][i] === 1) {
+        if (cards[i] === 1) {
           cardList.push(i);
         }
       }
