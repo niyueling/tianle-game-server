b75e23878406aa2e9965c5cfad6fafdd5dc7fa56
我爱蓝莓 <56059199@qq.com>
2024-06-11 16:17:26 +0800

    测试重连

--- src/match/xmmajiang/player_state.ts
+++ src/match/xmmajiang/player_state.ts
@@ -642,6 +642,7 @@ class PlayerState implements Serializable {
     this.cards.takeSelfCard = true
     this.cards.qiaoXiang = this.hadQiaoXiang
     this.cards.first = this.turn === 2
+    console.warn("cards-%s", JSON.stringify(this.getCardsArray()));
     const result = HuPaiDetect.check(this.cards, this.events, this.rule, this.seatIndex);
     result.hu = this.isDoubleGoldCardForYouJin(result);
     return result;
