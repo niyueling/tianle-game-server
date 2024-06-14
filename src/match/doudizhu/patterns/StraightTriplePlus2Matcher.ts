66aafd0c629a304866b49cd697df09544ec86600
我爱蓝莓 <56059199@qq.com>
2024-06-14 13:31:42 +0800

    斗地主发牌接入测试工具

--- src/match/doudizhu/patterns/StraightTriplePlus2Matcher.ts
+++ src/match/doudizhu/patterns/StraightTriplePlus2Matcher.ts
@@ -51,40 +51,38 @@ export default class StraightTriplePlus2Matcher implements IMatcher {
 
   promptWithPattern(target: IPattern, cards: Card[]): Card[][] {
     if (!target.name.startsWith(this.name) || target.cards.length > cards.length) {
-      return []
+      return [];
     }
     const triples = parseInt(target.name.replace('triples++_', ''), 10) || 0
-    if (triples <= 1)
-      return []
+    if (triples <= 1) {
+      return [];
+    }
+
     const tripleLen = triples * 3
 
     const tripleGroups = groupBy(cards.filter(c => c.point > target.score && c.point < Enums.c2.point), c => c.point)
       .filter(grp => grp.length >= 3)
-      .sort((g1, g2) => g1[0].point - g2[0].point)
+      .sort((g1, g2) => g1[0].point - g2[0].point);
 
-    const prompts = []
+    const prompts = [];
 
-    let start = 0
+    let start = 0;
     while (start < tripleGroups.length) {
-      const prompt = [...tripleGroups[start].slice(0, 3)]
-
-      let prevGroup = tripleGroups[start]
+      const prompt = [...tripleGroups[start].slice(0, 3)];
+      let prevGroup = tripleGroups[start];
       for (let i = start + 1; i < tripleGroups.length; i++) {
-        const currentGroup = tripleGroups[i].slice(0, 3)
+        const currentGroup = tripleGroups[i].slice(0, 3);
 
         if (currentGroup[0].point - prevGroup[0].point === 1) {
-          prevGroup = currentGroup
-          prompt.push(...currentGroup)
+          prevGroup = currentGroup;
+          prompt.push(...currentGroup);
 
           if (prompt.length === tripleLen) {
-            const leftCards = [].concat(...groupBy(arraySubtract(cards, prompt), card => card.point)
-              .sort(lengthFirstThenPointGroupComparator))
-
-            prompts.push([...prompt, ...leftCards.slice(0, triples * 2)])
+            const leftCards = [].concat(...groupBy(arraySubtract(cards, prompt), card => card.point).sort(lengthFirstThenPointGroupComparator));
+            prompts.push([...prompt, ...leftCards.slice(0, triples)]);
             break;
-
           } else {
-            start = i - 1
+            start = i - 1;
           }
         }
       }
@@ -93,13 +91,6 @@ export default class StraightTriplePlus2Matcher implements IMatcher {
     }
 
     return prompts
-    // map(group => {
-    //   const triple = group.slice(0, 3)
-    //   const leftCards = [].concat(...groupBy(arraySubtract(cards, triple), c => c.point)
-    //     .sort(lengthFirstThenPointGroupComparator))
-    //
-    //   return [...triple, leftCards[0], leftCards[1]]
-    // })
   }
 
 }
