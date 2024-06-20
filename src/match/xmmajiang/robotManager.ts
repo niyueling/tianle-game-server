b92251d22adf3465e1d543759340fd9f9e36c8bc
我爱蓝莓 <56059199@qq.com>
2024-06-20 08:48:33 +0800

    十二星座金豆房增加币种配置，结算按照币种扣除

--- src/match/xmmajiang/robotManager.ts
+++ src/match/xmmajiang/robotManager.ts
@@ -151,7 +151,7 @@ export class RobotManager extends NewRobotManager {
       }
     }
 
-    if (this.room.gameState.stateData && this.room.gameState.stateData.chi && this.room.gameState.stateData.peng && !pengStatus) {
+    if (this.room.gameState.stateData && this.room.gameState.stateData.chi && this.room.gameState.stateData.peng) {
       console.warn("action-%s, _id-%s", Enums.peng, this.room.gameState.stateData[Enums.peng]._id);
       return false;
     }
