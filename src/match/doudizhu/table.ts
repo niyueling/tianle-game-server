ccf158fca876c1d481ed03ff36e5260ab49bc500
我爱蓝莓 <56059199@qq.com>
2024-07-04 09:42:40 +0800

    地主牌接口下发倍数

--- src/match/doudizhu/table.ts
+++ src/match/doudizhu/table.ts
@@ -406,7 +406,7 @@ abstract class Table implements Serializable {
       // 将地主牌发给用户
       const cards = this.cardManager.getLandlordCard();
       this.players[firstLandlordIndex].cards = [...this.players[firstLandlordIndex].cards, ...cards];
-      this.room.broadcast("game/openLandlordCard", {ok: true, data: {seatIndex: this.players[firstLandlordIndex].index, landlordCards: cards, cards: this.players[firstLandlordIndex].cards}});
+      this.room.broadcast("game/openLandlordCard", {ok: true, data: {seatIndex: this.players[firstLandlordIndex].index, landlordCards: cards, multiple: this.multiple}});
 
       const startDaFunc = async() => {
         this.status.current.seatIndex = this.players[firstLandlordIndex].index;
@@ -509,7 +509,7 @@ abstract class Table implements Serializable {
         // 将地主牌发给用户
         const cards = this.cardManager.getLandlordCard();
         this.players[firstLandlordIndex].cards = [...this.players[firstLandlordIndex].cards, ...cards];
-        this.room.broadcast("game/openLandlordCard", {ok: true, data: {seatIndex: this.players[firstLandlordIndex].index, landlordCards: cards}});
+        this.room.broadcast("game/openLandlordCard", {ok: true, data: {seatIndex: this.players[firstLandlordIndex].index, multiple: this.multiple, landlordCards: cards}});
 
         const startDaFunc = async() => {
           this.status.current.seatIndex = this.players[firstLandlordIndex].index;
