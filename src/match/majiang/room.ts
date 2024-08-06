4f9553342f3b4dc95727621e243dc03dcf868bfc
我爱蓝莓 <56059199@qq.com>
2024-08-06 10:00:58 +0800

    血流红中修复战绩不显示

--- src/match/majiang/room.ts
+++ src/match/majiang/room.ts
@@ -574,7 +574,7 @@ class Room extends RoomBase {
     }
 
     const newModel = {...newJoinPlayer.model, medalId, headerBorderId};
-    const index = this.players.findIndex(p => p && !p.isRobot);
+    const index = this.players.findIndex(p => !p.isRobot);
 
     return {
       _id: this._id,
