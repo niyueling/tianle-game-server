8fa94a6653518a1a9da928620333f4db84e9f32b
我爱蓝莓 <56059199@qq.com>
2024-08-07 14:26:36 +0800

    测试血流妙手回春玩法

--- src/match/xueliu/table_state.ts
+++ src/match/xueliu/table_state.ts
@@ -2306,7 +2306,7 @@ class TableState implements Serializable {
 
   async checkMiaoShouHuiChun(player) {
     const isZiMo = player.zimo(this.lastTakeCard, this.turn === 1, this.remainCards === 0);
-    console.warn("isZiMo-%s, remainCards-%s", isZiMo, this.remainCards);
+    console.warn("isZiMo-%s, remainCards-%s, flag-%s", isZiMo, this.remainCards, this.remainCards === 0 && isZiMo);
     return this.remainCards === 0 && isZiMo;
   }
 
