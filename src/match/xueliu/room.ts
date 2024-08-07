6609093ae6002cf7ac164e56b3d7ffb9cd07903f
我爱蓝莓 <56059199@qq.com>
2024-08-07 16:39:18 +0800

    在线用户超过10秒没有点击继续就踢出局

--- src/match/xueliu/room.ts
+++ src/match/xueliu/room.ts
@@ -799,7 +799,7 @@ class Room extends RoomBase {
     }
 
     p.room = null
-    this.broadcast('room/leaveReply', {ok: true, data: {playerId: p._id.toString(), roomId: this._id}})
+    this.broadcast('room/leaveReply', {ok: true, data: {playerId: p._id.toString(), roomId: this._id, index: player.seatIndex}})
     this.removeReadyPlayer(p._id.toString())
     this.clearScore(player._id.toString())
 
