525c12f3cd43e18fa46042fe98d65b75a41eac94
我爱蓝莓 <56059199@qq.com>
2024-08-15 14:41:49 +0800

    修复炸弹无法准备

--- src/match/zhadan/robotManager.ts
+++ src/match/zhadan/robotManager.ts
@@ -16,15 +16,17 @@ export class RobotManager extends NewRobotManager {
     // 检查是否准备好
     let isOk;
     if (this.model.step === RobotStep.start) {
+      // 离线用户准备
+      const flag = await this.robotPlayerReady();
       isOk = await this.isHumanPlayerReady();
       if (!isOk) {
-        console.log('human player not ready', this.room._id);
+        // console.log(`human player not ready`, this.room._id);
         return;
       }
-      this.model.step = RobotStep.checkCardReady;
-      await this.save();
-      // 离线用户准备
-      await this.robotPlayerReady();
+      if ((flag && this.room.isPublic) || !this.room.isPublic) {
+        this.model.step = RobotStep.checkCardReady;
+        await this.save();
+      }
     }
     if (this.model.step === RobotStep.selectMode) {
       // 选择模式
