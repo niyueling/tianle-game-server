47bb094ec1aa590819a8df2d8fc123afec5c66dd
我爱蓝莓 <56059199@qq.com>
2024-09-12 09:31:04 +0800

    厦门麻将新手保护发好牌

--- src/match/xmmajiang/table_state.ts
+++ src/match/xmmajiang/table_state.ts
@@ -540,59 +540,59 @@ class TableState implements Serializable {
     }
 
     let residueCount = numbers - cards.length;
-    if (residueCount >= 2) {
-      // 还需要补牌超过2张，则补一个对子，或者补顺子之二
-      const doubleRandom = Math.random() < 0.5;
-      let result = [];
-      residueCount -= 2;
-
-      // 发对子
-      if (doubleRandom) {
-        result = Object.keys(counter).filter(num => counter[num] >= 2);
-        const randomNumber = Math.floor(Math.random() * result.length);
-        // 摸牌时摸到对子的牌
-        // this.testMoCards.push(Number(result[randomNumber]));
-        for (let i = 0; i < 2; i++) {
-          const index = this.cards.findIndex(card => card === Number(result[randomNumber]));
-
-          if (index !== -1) {
-            const card = this.cards[index];
-            cards.push(card);
-            this.cards.splice(index, 1);
-            this.lastTakeCard = card;
-            this.remainCards--;
-            counter[card]--;
-          }
-        }
-      } else {
-        // 发放顺子之二
-        result = Object.keys(counter).filter(num => Number(num) <= Enums.tongzi7 && counter[num] >= 1 && counter[Number(num) + 1] >= 1);
-        const randomNumber = Math.floor(Math.random() * result.length);
-        // 摸牌时摸到顺子的牌
-        // this.testMoCards.push(Number(result[randomNumber]) + 2);
-        for (let i = 0; i < 2; i++) {
-          const index = this.cards.findIndex(card => card === Number(result[randomNumber]) + i);
-          if (index !== -1) {
-            const card = this.cards[index];
-            cards.push(card);
-            this.cards.splice(index, 1);
-            this.lastTakeCard = card;
-            this.remainCards--;
-            counter[card]--;
-          }
-        }
-      }
-    }
-
-    // 检测是否发牌完成
-    if (residueCount > 0) {
-      let cardIndex = --this.remainCards;
-      const card = this.cards[cardIndex];
-      cards.push(card);
-      this.cards.splice(cardIndex, 1);
-      this.lastTakeCard = card;
-      counter[card]--;
-    }
+    // if (residueCount >= 2) {
+    //   // 还需要补牌超过2张，则补一个对子，或者补顺子之二
+    //   const doubleRandom = Math.random() < 0.5;
+    //   let result = [];
+    //   residueCount -= 2;
+    //
+    //   // 发对子
+    //   if (doubleRandom) {
+    //     result = Object.keys(counter).filter(num => counter[num] >= 2);
+    //     const randomNumber = Math.floor(Math.random() * result.length);
+    //     // 摸牌时摸到对子的牌
+    //     // this.testMoCards.push(Number(result[randomNumber]));
+    //     for (let i = 0; i < 2; i++) {
+    //       const index = this.cards.findIndex(card => card === Number(result[randomNumber]));
+    //
+    //       if (index !== -1) {
+    //         const card = this.cards[index];
+    //         cards.push(card);
+    //         this.cards.splice(index, 1);
+    //         this.lastTakeCard = card;
+    //         this.remainCards--;
+    //         counter[card]--;
+    //       }
+    //     }
+    //   } else {
+    //     // 发放顺子之二
+    //     result = Object.keys(counter).filter(num => Number(num) <= Enums.tongzi7 && counter[num] >= 1 && counter[Number(num) + 1] >= 1);
+    //     const randomNumber = Math.floor(Math.random() * result.length);
+    //     // 摸牌时摸到顺子的牌
+    //     // this.testMoCards.push(Number(result[randomNumber]) + 2);
+    //     for (let i = 0; i < 2; i++) {
+    //       const index = this.cards.findIndex(card => card === Number(result[randomNumber]) + i);
+    //       if (index !== -1) {
+    //         const card = this.cards[index];
+    //         cards.push(card);
+    //         this.cards.splice(index, 1);
+    //         this.lastTakeCard = card;
+    //         this.remainCards--;
+    //         counter[card]--;
+    //       }
+    //     }
+    //   }
+    // }
+    //
+    // // 检测是否发牌完成
+    // if (residueCount > 0) {
+    //   let cardIndex = --this.remainCards;
+    //   const card = this.cards[cardIndex];
+    //   cards.push(card);
+    //   this.cards.splice(cardIndex, 1);
+    //   this.lastTakeCard = card;
+    //   counter[card]--;
+    // }
 
     return cards;
   }
