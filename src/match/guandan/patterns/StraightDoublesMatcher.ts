80c88627c57d5e5dcee97c27b8027281e23eb251
逆月翎 <1061451899@qq.com>
2024-10-29 16:52:39 +0800

    掼蛋连对牌型适配A最小钢板

--- src/match/guandan/patterns/StraightDoublesMatcher.ts
+++ src/match/guandan/patterns/StraightDoublesMatcher.ts
@@ -6,16 +6,21 @@ export default class StraightDoublesMatcher implements IMatcher {
 
   verify(cards: Card[]): IPattern | null {
     if (cards.length === 6) {
-
       const sortedGroups = groupBy(cards.slice(), card => card.point)
         .sort((grp1, grp2) => {
           return grp1[0].point - grp2[0].point
         })
+      let result = {
+        name: PatterNames.doubles + sortedGroups.length,
+        score: sortedGroups[0][0].point,
+        cards,
+        level: sortedGroups.length
+      }
 
-      if (last(sortedGroups)[0].point >= 15) return null
+      if (last(sortedGroups)[0].point >= 15) result = null
 
       if (!sortedGroups.every(grp => grp.length === 2)) {
-        return null
+        result = null
       }
 
       let prevGroup = sortedGroups[0]
@@ -24,9 +29,33 @@ export default class StraightDoublesMatcher implements IMatcher {
         if (currentGroup[0].point - prevGroup[0].point === 1) {
           prevGroup = currentGroup
         } else {
+          result = null
+        }
+      }
+
+      if (result) {
+        return result;
+      }
+
+      const sortedGroupsByValue = groupBy(cards.slice(), card => card.value)
+        .sort((grp1, grp2) => {
+          return grp1[0].value - grp2[0].value
+        })
+
+      if (last(sortedGroupsByValue)[0].value > 13) {
+        return null;
+      }
+
+      let prevGroup1 = sortedGroupsByValue[0]
+      for (let i = 1; i < sortedGroupsByValue.length; i++) {
+        const currentGroup = sortedGroupsByValue[i]
+        if (currentGroup[0].point - prevGroup1[0].point === 1) {
+          prevGroup1 = currentGroup
+        } else {
           return null
         }
       }
+
       return {
         name: PatterNames.doubles + sortedGroups.length,
         score: sortedGroups[0][0].point,
@@ -77,7 +106,44 @@ export default class StraightDoublesMatcher implements IMatcher {
       } else {
         i = j
       }
+    }
 
+    if (prompts.length) {
+      return prompts;
+    }
+
+    const groupsByValue = groupBy(
+      cards.filter(c => c.value > target.score && c.value <= Enums.c13.value),
+      card => card.value)
+      .filter(g => g.length >= 2 && g.length < 4)
+      .sort((grp1, grp2) => {
+        return grp1[0].value - grp2[0].value
+      })
+
+    for (let i = 0; i < groupsByValue.length;) {
+      let prevCard = groupsByValue[i][0]
+      const prompt = [...groupsByValue[i].slice(0, 2)]
+
+      let j = i + 1
+      for (; j < groupsByValue.length; j++) {
+        const nextCard = groupsByValue[j][0]
+        if (nextCard.value - prevCard.value === 1) {
+          prevCard = nextCard
+          prompt.push(...groupsByValue[j].slice(0, 2))
+          if (prompt.length === len) {
+            break
+          }
+        } else {
+          break
+        }
+      }
+
+      if (prompt.length === len) {
+        i++
+        prompts.push(prompt)
+      } else {
+        i = j
+      }
     }
 
     return prompts
