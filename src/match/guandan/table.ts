01d42e2c0992b76104077123e68c363a2e99e560
逆月翎 <1061451899@qq.com>
2024-11-11 11:19:35 +0800

    测试工具发牌，级牌固定为2

--- src/match/guandan/table.ts
+++ src/match/guandan/table.ts
@@ -283,6 +283,12 @@ abstract class Table implements Serializable {
   }
 
   async fapai(payload) {
+    payload.cards = [
+      [{type: 2, point: 2, value: 2}, {type: 2, point: 2, value: 2}],
+      [],
+      [],
+      []
+    ];
     await this._fapai(payload)
 
     // 分配队友
