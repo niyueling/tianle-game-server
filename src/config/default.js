e730b851e90d649293c2394c2c7b32f08f1053aa
逆月翎 <1061451899@qq.com>
2024-11-13 20:42:51 +0800

    掼蛋修改三次不过回2逻辑

--- src/config/default.js
+++ src/config/default.js
@@ -73,7 +73,7 @@ module.exports = {
     message: true
   },
   game: {
-    dissolveTime: 1000 * 60 * 15,
+    dissolveTime: 1000 * 60 * 30,
     helpCount: 5,
     initModelGoldCount: 0,
     luckyDrawNeedGold: 500,
