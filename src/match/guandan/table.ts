fcff841d2ef7309ff6a88a9bba8cf28f5454cd35
逆月翎 <1061451899@qq.com>
2024-11-13 19:58:40 +0800

    掼蛋增加炸弹翻倍

--- src/match/guandan/table.ts
+++ src/match/guandan/table.ts
@@ -477,7 +477,7 @@ abstract class Table implements Serializable {
     const primaryDelayTime = playerIsOndeposit ? 1500 : time * 1000;
     const delayTime = primaryDelayTime - (Date.now() - this.autoCommitStartTime);
     this.autoCommitTimer = setTimeout(async () => {
-      console.warn("tableState %s  currentSearIndex %s", this.tableState, this.currentPlayerStep);
+      console.warn("tableState %s step %s currentSearIndex %s", this.tableState, this.room.robotManager.model.step, this.currentPlayerStep);
       if (this.tableState === "selectMode") {
         return await this.autoCommitForPlayerChooseMode();
       } else if (this.tableState === "returnTribute") {
