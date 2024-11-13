bc1ff2424f66d8036172158186d268dc0944a435
逆月翎 <1061451899@qq.com>
2024-11-13 20:00:02 +0800

    掼蛋增加炸弹翻倍

--- src/match/guandan/Rule.ts
+++ src/match/guandan/Rule.ts
@@ -41,12 +41,13 @@ class Rule {
 
   // 是否允许加倍
   get allowDouble() {
-    return this.ro.allowDouble || false;
+    // return this.ro.allowDouble || false;
+    return false;
   }
 
   // 炸弹允许翻倍
   get allowBombDouble() {
-    return this.ro.allowBombDouble || false;
+    return this.ro.allowDouble || false;
   }
 
   // 发牌，随即发牌：1， 不洗牌：2
