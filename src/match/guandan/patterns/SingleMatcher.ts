7c59fbb51cf1b3f26726a8b8b5cdccccdef16beb
逆月翎 <1061451899@qq.com>
2024-11-21 17:24:00 +0800

    掼蛋修复不用大小王吃单张

--- src/match/guandan/patterns/SingleMatcher.ts
+++ src/match/guandan/patterns/SingleMatcher.ts
@@ -14,20 +14,8 @@ export default class SingleMatcher implements IMatcher {
   }
 
   promptWithPattern(target: IPattern, cards: Card[], levelCard?: Number): Card[][] {
-    // 炸弹包括普通炸弹和王炸弹
-    const haveBomb = groupBy(cards, card => card.point)
-      .filter(g => g.length >= 4).length > 0 || cards.filter(c => c.point >= 16).length >= 4;
-
-    const haveBombFilter = function (g: Card[]) {
-      return g.length < 4 && g[0].point < 16
-    }
-    const noBombFilter = function (g: Card[]) {
-      return g.length < 4;
-    }
-    const filterFun = haveBomb ? haveBombFilter : noBombFilter;
-
     return groupBy(cards.filter(c => c.point > target.score), card => card.point)
-    .filter(filterFun)
+    .filter(g => g.length < 4)
       .sort(lengthFirstThenPointGroupComparator)
       .map(grp => [grp[0]])
   }
