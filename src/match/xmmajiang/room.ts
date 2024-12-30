e2b732dfc122ebe8cf54584c4726c22fdd9d5937
逆月翎 <1061451899@qq.com>
2024-12-30 11:16:02 +0800

    调整机器人加入房间节奏

--- src/match/xmmajiang/room.ts
+++ src/match/xmmajiang/room.ts
@@ -168,7 +168,7 @@ class Room extends RoomBase {
       await this.playerDisconnect(disconnectPlayer)
     }
 
-    this.init();
+    // this.init();
 
     this.readyPlayers = []
     this.gameState = null
