50cdb86e0f368b5bb19a8ab616e3c2454bb5b84b
逆月翎 <1061451899@qq.com>
2025-01-02 09:32:21 +0800

    修复掼蛋可用炸弹吃同花顺

--- src/match/guandan/patterns.ts
+++ src/match/guandan/patterns.ts
@@ -159,6 +159,10 @@ class Pattern {
         return foundPattern;
       }
 
+      if (pattern.name === PatterNames.straightFlush + 5) {
+        console.warn("pattern-%s foundPattern-%s", JSON.stringify(pattern), JSON.stringify(foundPattern));
+      }
+
       // 同花顺，必须6星炸弹才能吃牌
       if (pattern.name === PatterNames.straightFlush + 5 && foundPattern.name === PatterNames.bomb && foundPattern.level > 5) {
         return foundPattern;
