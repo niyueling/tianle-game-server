1ef6c5c64ec77fc7d9e1b63978ba80d556616c85
逆月翎 <1061451899@qq.com>
2025-01-13 11:54:03 +0800

    增加红包提现接口

--- src/match/redpocket/table_state.ts
+++ src/match/redpocket/table_state.ts
@@ -1148,7 +1148,7 @@ class TableState implements Serializable {
               this.cardTypes = await this.getCardTypes(player, 1);
               player.cards[nextCard]--;
 
-              const msg = player.canReceive(this.turn, nextCard,
+              const msg = player.gangTakeCard(this.turn, nextCard,
                 {
                   id: this.cardTypes.cardId,
                   multiple: await this.getRoomMultiple(player)
